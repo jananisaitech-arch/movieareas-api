72086f1c334d4f748d755ec7db6a575f6aea540a
Janani <janani.saitech@email.com>
2026-09-08 12:23:51 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ app.get('/', (req , res) => {
   console.log("Hello World!");
 })
 
+// CRUD functionalities of movies
+
+// reading movies
+app.get('/movies', (req, res) => {
+  
+})
+
+// creating movies
+app.post('/movies', (req, res) => {
+
+})
+
+// updating movie
+app.put('/movies/:id', (req, res) => {
+
+})
+
+// deleting movie
+app.delete('/movies/:id', (req, res) => {
+  
+})
+
 app.listen( PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 })
